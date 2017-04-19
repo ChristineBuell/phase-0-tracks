6e28be71581fe953c6f1f7aed09ed43de631e122
Chris Buell <christine.buell@gmail.com>
2017-04-18 21:19:30 -0700

    initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+puts "What is your name?"
+name = gets.chomp()
+puts "How old are you?"
+age = gets.chomp()
+puts "What year were you born?"
+birth_year = gets.chomp()
+puts "Our company cafeteria serves garlic bread. Should we order you some? (y/n)"
+garlic = gets.chomp()
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+insurance = gets.chomp()
+
+puts "Your answers were name: #{name}, age: #{age}, you were born in: #{birth_year}, garlic: #{garlic}, and insurance: #{insurance}."
