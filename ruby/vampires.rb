152c616b3a2c60c1671e4e65d1b70911e24622f2
Chris Buell <christine.buell@gmail.com>
2017-04-19 20:08:20 -0700

    updated logic. Drill down incomplete still

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,12 @@
+#Current year for calculations
 this_year = 2017
+
+#Check for how many employees to screen
+puts "How many employees are being entered?"
+number = gets.chomp.to_i
+
+#Loop through the form 
+while number > 0
 
 puts "What is your name?"
 name = gets.chomp()
@@ -6,6 +14,8 @@ puts "How old are you?"
 age = gets.chomp().to_i
 puts "What year were you born?"
 birth_year = gets.chomp().to_i
+
+#Make sure the data entered in proper format
 puts "Our company cafeteria serves garlic bread. Should we order you some? (y/n)"
 garlic = gets.chomp()
 	if garlic.downcase != "y" && garlic.downcase != "n"
@@ -13,7 +23,9 @@ garlic = gets.chomp()
 		puts "You need to enter (y/n)"
 		garlic = gets.chomp()
 		end until garlic.downcase == "y" || garlic.downcase == "n"
-	end 		
+	end 	
+
+#Make sure the data entered in proper format	
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp()
 	if insurance.downcase != "y" && insurance.downcase != "n"
@@ -23,7 +35,21 @@ insurance = gets.chomp()
 		end until insurance.downcase == "y" || insurance.downcase == "n"
 	end 		
 
-
+#Check for allergies
+puts "Please tell what allergies you have"
+puts "When you are finished type the word done"
+is_vampire = nil
+allergy = gets.chomp()
+while allergy.downcase != "done"
+	#Answering sunshine stops loop
+	if allergy.downcase == "sunshine"
+		is_vampire = true
+		allergy = "done"
+	else
+		puts "Tell me another allergy you have."
+		allergy = gets.chomp()
+	end
+end
 
 # Check to see if age checks out!
 if this_year - birth_year == age
@@ -32,8 +58,8 @@ else
 	age_check = false
 end		
 
-#Vampire Drill Down
-	if name == "Drake Cula" || name == "Tu Fang"
+#Vampire Drill Down!
+	if name == "Drake Cula" || name == "Tu Fang" || is_vampire == true
 		print "Definitely a vampire"
 	elsif age_check == false && (garlic == "n" && insurance == "n")
 		puts "Almost certainly a vampire"
@@ -45,9 +71,12 @@ end
 		puts "Results inconclusive"	
 	end
 
+#decrement counter
+number -= 1
+end
+ puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends. :-)" 
 
-puts "Your answers were name: #{name}, age: #{age}, you were born in: #{birth_year}, garlic: #{garlic}, and insurance: #{insurance}."
-puts age_check
+
 
 
 
