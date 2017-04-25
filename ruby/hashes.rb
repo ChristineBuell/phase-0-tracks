860ede96175c6be2b40f59a1c0953a0eaf8709e0
Chris Buell <christine.buell@gmail.com>
2017-04-24 22:24:42 -0700

    First commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,20 @@
+#get client data from designer
+#After done, print back what user has entered
+#Ask for changes to information
+#loop through until 'none'
+#Print final version and exit
+
+client_info = {
+	name: "",
+	age: "",
+	num_children: "",
+	theme: ""
+	color_family: "",
+
+
+}
+
+
+
+
+puts "Client Name"
