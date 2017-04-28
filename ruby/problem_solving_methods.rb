4bbdbbcfaeb202407329539d43924277b17a9d24
Chris Buell <christine.buell@gmail.com>
2017-04-28 11:27:54 -0700

    First commit, release 0

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,23 @@
+# simple search
+# Release 0
+
+arr = [42, 89, 23, 1]
+
+def search_array (arr, x )
+counter = 0
+length = arr.length
+while counter < length
+	if arr[counter] == x 
+		return counter
+
+	end 
+	counter += 1
+end
+end
+x = 23
+p search_array(arr, x)
+
+x = 45
+p search_array(arr, x)
+
+
