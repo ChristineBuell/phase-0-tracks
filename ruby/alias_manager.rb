ec2b4ee9b49ddc79ecc9d3da5f588d2f2779f987
Chris Buell <christine.buell@gmail.com>
2017-04-27 19:50:57 -0700

    letter changing logic works

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,37 +6,79 @@ Change letters
 	swap consonants for the the next consonant 
 #2 arrays for final printout 
 =end
+puts "Enter a name to encode"
+new_name = gets.chomp.downcase
 
-vowels = "aeiou".chars 
-consonants = "bcdfghjklmnpqrstvwxyz".chars 
-
-name = "bob".split('')
-consonant_list = 21
-vowel_list = 5
+vowels = "aeiou".split('') 
+consonants = "bcdfghjklmnpqrstvwxyz".split('')
+old_name = "bubba"
+old_name += "smith"
+name = new_name.split('')
+vowel_count = 5
+consonant_count = 21
 n_index = 0
 v_index = 0
 c_index = 0
+v_flag = false
+count = name.count
 
-while n_index < 3
- while v_index < vowel_list 
 
+puts old_name
+while n_index < count
+	puts "at top"
+	puts name[n_index] + vowels[v_index]
+ while v_index < vowel_count 
+ 	puts "top of in v < v count"
+ 	puts name[n_index] + vowels[v_index]	
  	if name[n_index] == vowels[v_index]
  		if name[n_index] == "u"
  			name[n_index] = "a"
+ 			break
  		else
+ 			puts "Im in else"
  			name[n_index] = vowels[v_index + 1]
-  			puts name[n_index]
+ 			break
  		end
- 
-
+ 			puts "after end of if/else"
+ 			puts name[n_index] + vowels[v_index]
+ 			
  	end
   	v_index += 1
-  	puts "v_index: #{v_index}"
 end
 n_index += 1
-puts "n_index: #{n_index}"
 v_index = 0
 end
+p name 
+#consonants
+
+n_index = 0
+
+while n_index < count
+	puts "at top"
+	puts name[n_index] + consonants[c_index]
+ while c_index < consonant_count 
+ 	puts "top of in c < c count"
+ 	puts name[n_index] + consonants[c_index]	
+ 	if name[n_index] == consonants[c_index]
+ 		if name[n_index] == "z"
+ 			name[n_index] = "b"
+ 			break
+ 		else
+ 			puts "Im in else"
+ 			name[n_index] = consonants[c_index + 1]
+ 			break
+ 		end
+ 			puts "after end of if/else"
+ 			puts name[n_index] + consonants[c_index]
+ 			
+ 	end
+  	c_index += 1
+end
+n_index += 1
+c_index = 0
+end
+puts name
+
 
 
 
