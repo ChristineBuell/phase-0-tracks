4049b7e75cd2fe85c8b33518edaa3364a3a6d300
Chris Buell <christine.buell@gmail.com>
2017-04-28 13:15:35 -0700

    final commit, bubble sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,6 +1,6 @@
 # simple search
 # Release 0
-=begin
+
 arr = [42, 89, 23, 1]
 
 def search_array (arr, x )
@@ -19,7 +19,7 @@ p search_array(arr, x)
 
 x = 45
 p search_array(arr, x)
-=end
+
 # Fibonacci
 # Release 1
 
@@ -48,6 +48,42 @@ if fib_array[-1] == fib_100
 else
 	puts "back to work!"
 end
+
+
+
+=begin 
+	Release 2
+	Bubble Sort
+	array of 5 unsorted integers
+	start at index 0 compare to next cell
+	if next cell is lower than first: swap
+	else leave and go to next cell
+	repeat until all cells are done	
+=end
+
+sort_me = [7, 3, 1, 9, 5]
+counter = sort_me.length
+
+a = 0
+b = 1
+p sort_me
+
+while a < (counter-1)
+	b = a+1
+	while b < (counter)
+		if sort_me[a] > sort_me[b]
+			temp = sort_me[a]
+			sort_me[a] = sort_me[b]
+			sort_me[b] = temp
+		end
+		b += 1
+	end
+	a += 1
+end
+p sort_me
+
+
+
 
 
 
