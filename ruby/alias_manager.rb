11fe0f12c3f8425321e6fe6994c8feb182909666
Chris Buell <christine.buell@gmail.com>
2017-04-28 10:56:14 -0700

    Almost done with arrays

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,88 +1,95 @@
 =begin
 Create alias:
 Swap names (first and last)
+Save names in original name array
+Take names one at a time
 Change letters
 	swap vowels for the next vowel
 	swap consonants for the the next consonant 
-#2 arrays for final printout 
+	save in code name array
+# 2 arrays for final printout loop by index
 =end
-puts "Enter a name to encode"
-new_name = gets.chomp.downcase
 
+# Real Name array
+real_name = Array.new(10) { Array.new(10) }
+# Code name array
+code_name = Array.new(10) { Array.new(10) }
+
+#vowel and consonant strings for comparisons
 vowels = "aeiou".split('') 
 consonants = "bcdfghjklmnpqrstvwxyz".split('')
-old_name = "bubba"
-old_name += "smith"
-name = new_name.split('')
+# Lengths of strings for comparisons
 vowel_count = 5
 consonant_count = 21
-n_index = 0
+input_index = 0
+
+#get first and last names, check for done
+puts "Enter a first name to encode or type done"
+new_name = gets.chomp.downcase
+while new_name != "done"
+puts input_index
+real_name[input_index][0] = new_name
+puts real_name[input_index][0]
+puts "Now enter a last name to encode"
+real_last_name = gets.chomp.downcase
+code_name[input_index][1] = real_last_name
+puts code_name[input_index][1]
+
+input_index += 1 
+
+
+name = new_name.split('')
+
+name_index = 0
 v_index = 0
 c_index = 0
-v_flag = false
 count = name.count
 
 
-puts old_name
-while n_index < count
-	puts "at top"
-	puts name[n_index] + vowels[v_index]
+# First loops through to change vowels
+while name_index < count
  while v_index < vowel_count 
- 	puts "top of in v < v count"
- 	puts name[n_index] + vowels[v_index]	
- 	if name[n_index] == vowels[v_index]
- 		if name[n_index] == "u"
- 			name[n_index] = "a"
+ 	
+ 	if name[name_index] == vowels[v_index]
+ 		if name[name_index] == "u"
+ 			name[name_index] = "a"
  			break
  		else
- 			puts "Im in else"
- 			name[n_index] = vowels[v_index + 1]
+  			name[name_index] = vowels[v_index + 1]
  			break
- 		end
- 			puts "after end of if/else"
- 			puts name[n_index] + vowels[v_index]
- 			
+ 		end	
  	end
   	v_index += 1
 end
-n_index += 1
+name_index += 1
 v_index = 0
 end
-p name 
-#consonants
 
-n_index = 0
+# Then loops through to change consonants
+name_index = 0
 
-while n_index < count
-	puts "at top"
-	puts name[n_index] + consonants[c_index]
+while name_index < count
  while c_index < consonant_count 
- 	puts "top of in c < c count"
- 	puts name[n_index] + consonants[c_index]	
- 	if name[n_index] == consonants[c_index]
- 		if name[n_index] == "z"
- 			name[n_index] = "b"
+ 	if name[name_index] == consonants[c_index]
+ 		if name[name_index] == "z"
+ 			name[name_index] = "b"
  			break
  		else
- 			puts "Im in else"
- 			name[n_index] = consonants[c_index + 1]
+ 			name[name_index] = consonants[c_index + 1]
  			break
  		end
- 			puts "after end of if/else"
- 			puts name[n_index] + consonants[c_index]
- 			
+ 			puts name[name_index] + consonants[c_index]
  	end
   	c_index += 1
 end
-n_index += 1
+name_index += 1
 c_index = 0
 end
-puts name
+puts name.join
+puts real_name[input_index][0]
+puts "Enter a first name to encode or type done"
+new_name = gets.chomp.downcase
+input_index += 1
+puts input_index
 
-
-
-
-
-
-
-
+end
