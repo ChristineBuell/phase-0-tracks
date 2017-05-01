9aba1b9c1939b4be173a8202924618cd5722213e
Chris Buell <christine.buell@gmail.com>
2017-05-01 12:50:12 -0700

    First and final commit

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+SPECIES-------------
+canine
+
+CHARACTERISTICS-----
+Size: varies
+Friendly: yes
+Name: varies
+Eye color: varies
+Coat color: varies
+Fur length: varies
+
+BEHAVIOR------------
+Bark
+Howl
+Sit
+Run
+Beg
+
+
+
