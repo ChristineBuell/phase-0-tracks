22b05316470ff11360c801b43d95fc3572a105af
Chris Buell <christine.buell@gmail.com>
2017-05-01 06:43:33 -0700

    solved a hash drill

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -38,16 +38,30 @@ print zombie_apocalypse_supplies
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
-# ----
+def check_list (find_item)
+  zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
+                              "shotgun", "compass", "CB radio", "batteries"]
+
+  zombie_apocalypse_supplies.each do |item|
+    if item == find_item
+      puts "#{item} is on the list!"
+    else 
+      puts "#{item} isn't on the list"
+    end 
+  end
+end 
+check_list("batteries")
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. Do not use any special built-in methods.
-=begin
-  
-rescue Exception => e
-  
-end
+
+=begin I wasn't sure if we could use .pop, so I added one more below.
+z_length = zombie_apocalypse_supplies.length 
+zombie_apocalypse_supplies.pop(z_length - 5)
+puts zombie_apocalypse_supplies
+=end 
+
 index = 0
 z_length = zombie_apocalypse_supplies.length
 while index < z_length
@@ -81,7 +95,7 @@ extinct_animals = {
 
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
-# ----
+
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
@@ -99,10 +113,26 @@ extinct_animals = {
 # "Dodo"
 # "Saiga Antelope"
 # Do not use any special built-in methods.
-# ----
+extinct_animals.each do |animal, year|
+ animals = ["Andean Cat", "Dodo", "Saiga Antelope"]
+ index = 0 
+  while index < animals.length 
+    if animal ==  animals[index]
+     puts "#{animal} is on the list!"
+    end 
+      index += 1 
+    end 
+  end
+
+ 
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
-# ----
+temp = {}
+temp = extinct_animals.select{|k,v| k == "Passenger Pigeon"}
+temp.merge!(extinct_animals){|k,v1,v2|v1}
+extinct_animals = temp
+extinct_animals.shift
+puts extinct_animals
