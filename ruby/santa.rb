af038ea7daeaf7c1e7cd18aedf5a24854aebba11
Chris Buell <christine.buell@gmail.com>
2017-05-01 20:11:31 -0700

    Initial commit

---
+++ ruby/santa.rb
@@ -0,0 +1,18 @@
+
+
+class Santa ()
+	def initialize)()
+	end
+	def speak()
+		puts "Ho, ho, ho! Haaappy holidays!"
+	end
+	def eat_milk_and_cookies(cookie) 
+		puts "That was a good #{cookie}"
+end
+end
+
+
+
+
+
+
