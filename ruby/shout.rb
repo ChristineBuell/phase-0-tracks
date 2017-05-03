2411e047e9a6a590f9bd9d5ded9056fca0cba959
Chris Buell <christine.buell@gmail.com>
2017-05-03 09:17:15 -0700

    got module running

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -15,4 +17,22 @@ Shout.yell_angrily("Get off my lawn")
 
 Shout.yelling_happily("Have a nice day")
 
-  
+=end
+
+module Shout
+	def yell_angrily(words)
+		puts words + "!!!" + " 8-(("
+	end
+
+	def yelling_happily(words)
+		puts words + "!" + " :D"
+	end
+end
+
+class Noise
+	include Shout
+end
+
+shout = Noise.new 
+shout.yelling_happily("Yay")
+shout.yell_angrily("Arrgh")
