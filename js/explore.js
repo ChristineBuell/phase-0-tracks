724d8a5611b8d104817c8b0bca4e83be92dade56
Chris Buell <christine.buell@gmail.com>
2017-05-08 15:36:34 -0700

    initial commit

---
+++ js/explore.js
@@ -0,0 +1,24 @@
+/*
+Function takes in a string
+Return reversed word using for loop
+initialize a second index at 0: j=0
+For string.length, i >= 0, i--
+	newStr[j] += str[i]
+	j++
+increment up j and down i	
+
+	
+
+
+
+
+
+*/
+
+
+function reverseWord
+
+	for (var i = str.length; i >= 0; i--){
+		var reversedString += str.substr(i, 1);
+
+	}
