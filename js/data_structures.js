1ee19e29fda8f6167e93cd9e5e1e6b35d398fa7c
Chris Buell <christine.buell@gmail.com>
2017-05-10 10:59:34 -0700

    final commit, release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -23,5 +23,31 @@ horse[horses[i]] = colors[i];
 console.log(horse)
 
 
+function Car(make, color, year, runs){
+
+	this.make = make;
+	this.color = color;
+	this.year = year;
+	this.runs = true;
+	this.honk = function() {console.log("beep beep!");};
+
+}
+
+var myCar = new Car("Mazda", "Red", 2015, true);
+console.log("Chris has a " + myCar.make + ".");
+console.log("Chris' car is "+ myCar.color + " and was made in " + myCar.year + ".")
+myCar.honk();
+console.log("My car is working?")
+console.log("-------------")
+var ikukoCar = new Car("Toyota", "Black", 1998, false);
+console.log("Ikuko has a " + ikukoCar.make + ".");
+console.log("Ikuko's car is "+ ikukoCar.color + " and was made in " + ikukoCar.year + ".")
+ikukoCar.honk();
+
+
+
+
+
+
 
 
