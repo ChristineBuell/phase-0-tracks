e1b446fc549d54b458d532552641b9e9cc2ee4d8
Chris Buell <christine.buell@gmail.com>
2017-05-10 09:26:24 -0700

    intial commit, release 1

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+
+var colors = ["red", "green", "purple", "blue"];
+var horses = ["Ed", "Spot", "Bob", "Jasmine"];
+
+colors.push("yellow");
+horses.push("Ikuko");
+
+console.log(colors);
+console.log(horses);
+
