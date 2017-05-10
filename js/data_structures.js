0bc18ab5612ac487d17cee29d60e4ce6cf95bf6d
Chris Buell <christine.buell@gmail.com>
2017-05-10 10:20:22 -0700

    release 1, last time was release 0

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,27 @@
 
-var colors = ["red", "green", "purple", "blue"];
-var horses = ["Ed", "Spot", "Bob", "Jasmine"];
+var colors = ["red", "magenta", "teal", "blue"];
+var horses = ["Ed", "Chris", "Bob", "Jasmine"];
 
-colors.push("yellow");
+colors.push("white");
 horses.push("Ikuko");
 
 console.log(colors);
 console.log(horses);
+
+var horse = {};
+var temp;
+
+
+for(var i = 0; i < horses.length; i++){
+
+horse[horses[i]] = colors[i];
+
+
+	
+};
+
+console.log(horse)
+
+
+
 
