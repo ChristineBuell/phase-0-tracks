99e58f488608dc2f8886a2d39cd4d8bdd136e465
Chris Buell <christine.buell@gmail.com>
2017-05-25 16:54:44 -0700

    Everything but the bonus challenge completed

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -41,34 +41,73 @@ end
 # write a GET route that retrieves
 # a particular student
 
+
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
 
+# Prints contact info for students (same address for each!)
 get '/contact' do 
-students = db.execute("SELECT * FROM students")
-contact_info = ""
-students.each do |student|
-contact_info << "#{student['name']}<br>"
-contact_info << "123 Somewhere lane <br>"
-contact_info << "Sequim, WA 98773<br> <br>"
-end
-contact_info
+  students = db.execute("SELECT * FROM students")
+  contact_info = ""
+  students.each do |student|
+  contact_info << "#{student['name']}<br>"
+  contact_info << "123 Somewhere lane <br>"
+  contact_info << "Sequim, WA 98773<br> <br>"
+  end
+  contact_info
 
 end
 
+# Prints great job "name" 
 get '/great_job' do
 
   name = params[:name]
-  if name
-    "Great Job #{name}!"
- else 
-  "Great Job!"
+    if name
+      "Great Job #{name}!"
+    else 
+      "Great Job!"
+    end
+  end
+
+# Adds two numbers and returns
+get '/add_two' do
+ 
+  result1 = params["num1"].to_i
+  result2 = params["num2"].to_i
+  final_result = result1 + result2
+  "#{params[:num1]} + #{params[:num2]} = #{final_result.to_s}"
 end
+
+get '/get_campus' do
+
+  place = params[:campus]
+  "Campus: #{params[:campus]}"
+  students.each do |student|
+  find_campus = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]][0])
+   "#{params[:name]} is at #{params[:campus]} campus."
+
 end
 
 
+
+
+
+ end
+
+=begin
+Release 1 
+
+Ruby web frameworks: Rails is the biggest one, Grape came after sinatra, Padrino which is based on 
+Sinatra and Cuba, which is a "micro framework".
+
+Databases that work with sinatra: MySQL, Mongo db, postreSQL, and some major ones like Oracle and DB2.
+
+A webstack is a group of softwar components needed for web development. LAMP is an example, it includes Linux, Apache, MySQL and PHP.   
+
+  
+=end
 
 
 
